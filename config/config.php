a5743b8655f59b1f2c0190ae9d090ebca5ffa59e
alice1311 <95027720+alice1311@users.noreply.github.com>
2023-12-23 19:17:19 +0700

    Delete config directory

--- config/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn=mysqli_connect("localhost","root","") or die ("Không thể kết nối đến server");
-$csdl=mysqli_select_db($conn,"handmadeshop") or die ("Không thể chọn được được csdl". mysqli_error($conn));
-mysqli_query($conn, "SET NAMES 'utf8'");
-?>
-
- 
